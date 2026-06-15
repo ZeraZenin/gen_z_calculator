5be045fbe8cedc55d1eb35853800dffdc529b18a
ZeraZenin <gutzchermain18@gmail.com>
2026-06-15 07:24:27 -0600

    gen_z_calculator_first

---
+++ gen_z_calculator.py
@@ -0,0 +1,34 @@
+#Gen Z Calculator
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+if operation == "+":
+    result = number1 + number2
+elif operation == "-":
+    result = number1 - number2
+elif operation == "*":
+    result = number1 * number2
+elif operation == "/":
+    result = number1 / number2
+else:
+    result = "Invalid operation"
+
+print("The result is:", result)
+
+if number1 == 7 and number2 == 8 and operation == "*":
+    print("Agarrame el chocho")
+if result == 13:
+    print("Entre mas me la mamas mas me crece hablale a un cura pa que me la rece que una como esta cualquiera merece ponte de rodillas pa que me la beses aunque este arrugada si me la endereces te doy con la fuerza de quinientas reces por toda la noche y hasta que me efervece te deja una bola de hasta nueve meses adentro de ti mi sazon prevalece y aqui en tu garganta se desaparece que hasta los pulmoneste los adormece")
+if result == 4:
+    print("En cuatro te pongo")
+if result == 420:
+    print("La hora adecuada")
+if result == 10:
+    print("Cupala pues")
+if result == 2:
+    print("duro dos horas haciendolo bien rico oh de las chicas soy su favorito wow todas dicen que me aman todas me quieren en su cama duro dos horas haciendolo bien rico oh de las chicas soy su favorito wow todas dicen que me aman todas me quieren en su cama la tengo en cuatro quiere que la rompa quiere maltrato en mi bicho se monta es una loca no se controla la quiere en su nuca para ella sola golosa se viene conmigo no le gustan los novatos pide que la folle en el patio de su casa pide leche fresca a cada rato se larga a la uni y follamos en el acto acto acto pide contacto acto acto pide contacto acto acto pide contacto pide que follemos cada rato")
+if number1 == 1 and number2 == 1 and operation == "+":
+    print("Me cojo a tu hermana")
+if result == 5:
+    print("por el culo te la hinco")
